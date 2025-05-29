e25bf81202487a3a479250166dcb93beda37ed8b
Belinda <bmdebruyn@gmail.com>
2025-05-29 09:22:11 -0600

    add missing colors to scheme

--- src/index.js
+++ src/index.js
@@ -193,7 +193,7 @@ var parseMetadata = metadata => {
                 lang: {
                     thousandsSep: ','
                 },
-                colors: ['#004b8d', '#47a5dc', '#faa834', '#00aa7e', '#006ac7', '#bf8028', '#00e4a7']
+                colors: ['#004b8d', '#939598', '#faa834', '#00aa7e', '#47a5dc', '#006ac7', '#ccced2', '#bf8028', '#00e4a7']
             });
 
             const chartOptions = {
